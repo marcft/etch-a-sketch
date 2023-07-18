0a73a26c7f9e0857f27823c55aa78396c68897c7
Marc Frances <marc.frances.t@gmail.com>
2023-07-18 17:22:31 +0200

    Add paint behaviour on the right side container
    We have added paint behaviour, in the left menu we can change between
    drawing modes by clicking the correspondig button, also we can adjust
    the grid size and clear the grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,21 +6,26 @@ const gridContainer = document.querySelector('.container');
 const modeButtons = document.querySelectorAll('button.mode');
 let currentActiveButton = document.querySelector('button.active');
 const colorPicker = document.querySelector('.color-picker > input');
+const clearButton = document.querySelector('button.clear');
 
 let mouseDown = false;
+let gridSize = 24;
 
 //Initializes the grid size
-newGridSize(24);
+newGridSize(gridSize);
 
 //Updates the grid size
 slider.addEventListener('input', () => {
-    const newSize = slider.value;
-    gridSizeText.textContent = `Grid Size: ${newSize} x ${newSize}`;
-    newGridSize(newSize);
+    gridSize = slider.value;
+    gridSizeText.textContent = `Grid Size: ${gridSize} x ${gridSize}`;
+    newGridSize(gridSize);
 });
 
 //Updates selected mode
 modeButtons.forEach((button) => button.addEventListener('click', changeActiveMode));
+
+//Clears the grid
+clearButton.addEventListener('click', () => { newGridSize(gridSize); })
 
 function changeActiveMode() {
     currentActiveButton.classList.remove('active');
@@ -40,6 +45,9 @@ function newGridSize(size) {
     const div = document.createElement('div');
     div.classList.add('square');
     div.setAttribute('style',`width:${divSide}px; height:${divSide}px`);
+    //Useful when shading
+    div.setAttribute('data-original-color','rgb(255, 255, 255)');
+    div.setAttribute('data-times-shaded','0');
 
     for (let i = 0; i < size * size; i++) {
         gridContainer.appendChild(div.cloneNode(true));
@@ -55,18 +63,68 @@ function newGridSize(size) {
 }
 
 function attachEventListeners(square) {
-    square.addEventListener('mousedown', (e) => { mouseDown = true; e.preventDefault()});
+    square.addEventListener('mousedown', (e) => { 
+        mouseDown = true; 
+        paintSquare(e.target); 
+        e.preventDefault();
+    });
     square.addEventListener('mouseup', () => { mouseDown = false });
     square.addEventListener('mouseover', (e) => {
-        if(mouseDown) {
-            if (currentActiveButton.classList.contains('normal')) {
-                paintNormal(e.target);
-            }
-        }
+        if(mouseDown) paintSquare(e.target);
     });
 }
 
-function paintNormal(target) {
-    const currentColorValue = colorPicker.value;
-    target.style.backgroundColor = currentColorValue;
+function paintSquare(target) {
+    if (currentActiveButton.classList.contains('normal')) {
+        const colorValue = colorPicker.value;
+        target.style.backgroundColor = colorValue;
+
+        const rgb = [parseInt(colorValue.substring(1,3), 16),
+                     parseInt(colorValue.substring(3,5), 16),
+                     parseInt(colorValue.substring(5), 16)];
+        target.dataset.originalColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    }
+
+    else if (currentActiveButton.classList.contains('rainbow')) {
+        const rgb = [0, 0, 0];
+        for (let i = 0; i < 3; i++) {
+            rgb[i] = Math.random() * 255;
+        }
+        const rgbString = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+        target.style.backgroundColor = rgbString;
+        target.dataset.originalColor = rgbString;
+    }
+
+    else if (currentActiveButton.classList.contains('shade')) {
+        let originalColor = target.dataset.originalColor;
+        originalColor = originalColor.substring(4, originalColor.length-1).replace(/ /g, '').split(',');
+        //Array with 3 numeric values
+        let newRgb = originalColor;
+        const timesShaded = target.dataset.timesShaded;
+        //We deminish each rgb value prportionally
+        for (let i = 0; i < 3; i++) {
+            const deminish = originalColor[i] / 10;
+            //If timeShaded is 10 it's completly black
+            if (timesShaded < 10) {
+                for (let j = 0; j <= timesShaded; j++) {
+                    if (newRgb[i] - deminish > 0) { 
+                        newRgb[i] = newRgb[i] - deminish;
+                    } else {
+                        //Prevent round errors
+                        newRgb[i] = 0;
+                    }
+                }
+            } else {
+                newRgb = [0, 0, 0];
+            }
+        }
+        target.style.backgroundColor = `rgb(${newRgb[0]}, ${newRgb[1]}, ${newRgb[2]})`;
+        target.dataset.timesShaded++;
+    }
+
+    else if (currentActiveButton.classList.contains('eraser')) {
+        target.style.backgroundColor = '#ffffff';
+        target.dataset.originalColor = 'rgb(255, 255, 255)';
+        target.dataset.timesShaded = 0;
+    }
 }
